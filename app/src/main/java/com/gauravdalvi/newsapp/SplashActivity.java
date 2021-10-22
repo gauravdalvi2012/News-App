40ce5b2572f85d6359c356b8798904e21f37c8a7
Gaurav Dalvi <gdalvi20dec@gmail.com>
2021-10-23 00:49:18 +0530

    added splash screen

---
+++ app/src/main/java/com/gauravdalvi/newsapp/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.gauravdalvi.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(1500);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+}
